dc548f8e9edf7ef72221656e2eaeade0aaacfbe1
strato-s <nigam2614@gmail.com>
2024-05-27 23:14:32 +0530

    initialization of project
    Trying to figure out how to render out the particles onto the webpage using canvas on HTML5

---
+++ script.js
@@ -0,0 +1,19 @@
+let N = 256;
+
+var canvas = document.getElementById('mainCanvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+function settings(){
+    
+}
+
+function setup(){
+
+}
+
+function draw(){
+
+}
